0872b099cba555792a99ed5ffbed2e14b16c1d4e
Axolotl-King2 <37072@lln.hethwc.nl>
2025-09-15 09:33:34 +0000

    modified:   2_1_Variabelen_1-4.py

--- 2_1_Variabelen_1-4.py
+++ 2_1_Variabelen_1-4.py
@@ -1,1 +1,12 @@
-# opgaven 1 tot en met 4 van paragraaf 2.1 Variabelen
+start = 'hello world im an AI'
+print(start)
+verzendkosten = 5
+productcode = 'A136i'
+print(verzendkosten, productcode)
+aantal = 5
+prijs = 9.95
+print(prijs,aantal)
+bedrag = '4'
+print(bedrag)
+bedrag = bedrag + 0.95
+print(bedrag)
