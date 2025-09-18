78a5a81063f116177c6dd587fa73e3e951780d76
Axolotl-King2 <37072@lln.hethwc.nl>
2025-09-18 07:36:48 +0000

    modified:   2_6_Variabelen_1-5.py 	modified:   2_7_Variabelen_1-2.py

--- 2_6_Variabelen_1-5.py
+++ 2_6_Variabelen_1-5.py
@@ -1,1 +1,23 @@
 # opdracht 1 tpt en met 5 van paragraaf 2.6
+# opdracht 1
+postcode = '1188EX'
+print(postcode.lower())
+
+# opdracht 2
+woonplaats = 'thalia'
+print(woonplaats.upper())
+
+# opdracht 3
+verzendmethode = 'postNL'
+betaalmethode = 'Ideal'
+print(verzendmethode.lower(), betaalmethode.upper())
+
+# opdracht 4
+#postcode = (str(input))
+#print(postcode.upper)
+postcode = input("Wat is je postcode?")
+print(postcode.upper())
+
+# opdracht 5
+plaatsnaam = '118exGasselternijveenschemond'
+print(len(plaatsnaam))
--- 2_7_Variabelen_1-2.py
+++ 2_7_Variabelen_1-2.py
@@ -1,1 +1,15 @@
 # opdracht 1 en 2 van paragraaf 2.7
+# opdracht 1
+voornaam = input('wat is je voornaam?') 
+achternaam = input('wat is je achternaam?')
+print('hello',voornaam, achternaam)
+
+# opdracht 2
+naam = input('wat is je naam?')
+straat = input('in welke straat woon je?')
+huisnummer = input('wat is je huisnummer?')
+postcode = input('wat is je postcode?')
+woonplaats = input('wat is je woonplaats?')
+print(naam)
+print(straat, huisnummer)
+print(postcode.upper(), woonplaats.upper())
