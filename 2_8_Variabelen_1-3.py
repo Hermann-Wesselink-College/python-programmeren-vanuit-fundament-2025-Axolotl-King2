1b176c2340c1f9f33c794be30c4e800d52c0c931
Axolotl-King2 <37072@lln.hethwc.nl>
2025-10-02 09:12:58 +0000

    modified:   2_8_Variabelen_1-3.py

--- 2_8_Variabelen_1-3.py
+++ 2_8_Variabelen_1-3.py
@@ -1,1 +1,9 @@
 # opdrachten 1 tot en met 3 van pragraaf 2.8
+prijs = 24,49
+print('prijs:' + str(prijs))
+
+voornaam = input('wat is je voornaam?')
+achternaam = input('wat is je achternaam?')
+print('mijn naam is' , str(voornaam), str(achternaam))
+naamlengte = voornaam + achternaam
+print('mijn naam bestaat uit', len(naamlengte), 'letters')casp
