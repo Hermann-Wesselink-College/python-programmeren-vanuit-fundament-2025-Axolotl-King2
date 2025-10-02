a4700b0defe8f2b9e959cbf49d60669e2973480d
Axolotl-King2 <37072@lln.hethwc.nl>
2025-10-02 06:31:42 +0000

    modified:   3_4_Selectie_1-9.py

--- 3_4_Selectie_1-9.py
+++ 3_4_Selectie_1-9.py
@@ -1,1 +1,9 @@
 # Opdrachten 1 tot en met 9 van paragraaf 3.4
+artikelen = int(input('how veel artikelen heeft u?'))
+oldprijs = int(input('wat is de prijs per artikel?'))
+prijs = artikelen*oldprijs
+if prijs > 150:
+	newprijs = prijs*1.19
+else:
+    newprijs = prijs*1.16
+print('de nieuwe prijs is = €', newprijs)
