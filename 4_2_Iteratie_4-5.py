ec646aaadddd30201e9a59396aeaaab1666e33a0
Axolotl-King2 <37072@lln.hethwc.nl>
2025-10-11 11:52:24 +0000

    modified:   4_2_Iteratie_4-5.py 	modified:   4_3_Iteratie_3-7.py

--- 4_2_Iteratie_4-5.py
+++ 4_2_Iteratie_4-5.py
@@ -1,1 +1,21 @@
-#Opdrachten 4 en 5 van paragraaf 4.2 
+#Opdracht 4
+jaar = 0
+saldo = 100
+while saldo < 400:
+    jaar = jaar + 1
+    saldo = saldo * 1.1
+print('jaar is: ' + str(jaar))
+print('saldo is: ' + str(saldo))
+
+#opdracht 5
+jaar2 = 0
+saldo2 = 100000
+opname = 5000
+while saldo2 <1000000:
+    if jaar2 >= 100:
+        print('Je kan dit bedrag je hele leven opnemen')
+        break
+    else:
+        saldo2 = saldo2 * 1.04 - opname
+        jaar2 = jaar2 + 1
+        print('dit kan ' + str(jaar2) + ' jaar')
--- 4_3_Iteratie_3-7.py
+++ 4_3_Iteratie_3-7.py
@@ -1,1 +1,50 @@
-# Opgave 3 tot en met 7 van paragraaf 4.3
+# Opgave 3
+# deel A
+for i in range(1,11):
+    print(i * 3)
+
+# deel B
+tafel = int(input('wat is de tafel die je gebruikt?'))
+for i in range(1,11):
+    print(tafel, ' x ', i, ' = ', i * tafel)
+
+# deel C
+tafel = int(input('wat is de tafel die je gebruikt?'))
+if tafel <= 10 or tafel >= 0:
+    for i in range(1,11):
+        print(tafel, ' x ', i, ' = ', i * tafel)
+else:
+    print('De tafel hoeft niet weergeven te worden')
+
+ # opdracht 4
+print('een \t tab')
+miles = 1.609
+print('km \t miles')
+for i in range(1,11):
+    print(i ,'\t', i*miles)
+
+#opdracht 5
+getal = int(input('van welk getal wil je de faculteit?'))
+faculteit = 1
+for i in range(1,getal + 1):
+    faculteit *= i
+print('de faculteit van', getal, 'is', faculteit)
+
+# opdracht 6
+for i in range(0,51,2):
+    print(i)
+
+# opdracht 7
+aantal = int(input('tot welk oneven getal wil je pi bereiken?'))
+pi = 0.0
+teken = 1
+for i in range(1, aantal + 1, 2):
+    pi += teken *(1/i)
+    teken *= -1
+pi *= 4
+print("Benaderde waarde van π is:", pi)
+
+# extra
+klaar = ['ik', 'ben', 'klaar']
+for i in klaar:
+    print(i)
