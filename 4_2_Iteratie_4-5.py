8fe3af3eac302b616ee415817d73b3cc4c5d894c
Axolotl-King2 <37072@lln.hethwc.nl>
2025-10-13 11:02:09 +0000

    modified:   4_2_Iteratie_4-5.py

--- 4_2_Iteratie_4-5.py
+++ 4_2_Iteratie_4-5.py
@@ -10,11 +10,13 @@ print('jaar is: ' + str(jaar), 'saldo is: ' + str(saldo))
 jaar2 = 0
 saldo2 = 100000
 opname = 5000
-while saldo2 <1000000:
+while ((saldo2 * 1.04) - opname) >0:
+    saldo *= 1.04
+    saldo -= opname
+    jaar += 1
     if jaar2 >= 100:
         print('Je kan dit bedrag je hele leven opnemen')
         break
     else:
-        saldo2 *= 1.04 - opname
-        jaar2 += 1
-print('dit kan ' + str(jaar2) + ' jaar')
+        print('dit kan ' + str(jaar2) + ' jaar')
+        break
